3e5f99f491442af0d4360ca9b345f22c45830ec8
Wilovent <william.klein@viacesi.fr>
2016-03-23 23:14:19 +0100

    mj

--- dist/app.js
+++ dist/app.js
@@ -70,11 +70,14 @@ angular.module('dashboard').component('time', {
     restrict: 'E',
     controller : ['$interval',function($interval){
         var that = this;
-        var updateTime = function(){
+        var updateTime = () => {
             now = new Date();
-            that.hours = now.getHours();
-            that.minutes = now.getMinutes();
-            that.secondes = ("0" + now.getSeconds()).slice(-2);
+            that.hours = toReadableTime(now.getHours());
+            that.minutes = toReadableTime(now.getMinutes());
+            that.secondes = toReadableTime(now.getSeconds())
+        };
+        var toReadableTime = (input) => {
+            return ("0" + input).slice(-2);
         }
         updateTime();
         $interval(updateTime,1000);
