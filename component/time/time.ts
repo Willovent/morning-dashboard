7d4025ee80169320fcd23c319596f6e9fd20aabd
Wilovent <william.klein@viacesi.fr>
2016-03-30 01:02:59 +0200

    format

--- component/time/time.ts
+++ component/time/time.ts
@@ -17,6 +17,8 @@ module Dashboard {
         controller = ['$interval', TimeController];
     }
 
+    angular.module('dashboard').component('time', new TimeComponent());
+
     angular.module('dashboard').filter('readableTime', () => (input: Date) => {
         let toReadableTime = (time: number) => ("0" + time).slice(-2);
         let hours = toReadableTime(input.getHours());
@@ -24,35 +26,33 @@ module Dashboard {
         // let secondes = toReadableTime(input.getSeconds());
         return `${hours}:${minutes}`;
     });
-    
+
     angular.module('dashboard').filter('dayOfWeek', () => (input: Date) => {
-        switch (input.getDay()){
-            case 1:return 'lundi';
-            case 2:return 'mardi';
-            case 3:return 'mercredi';
-            case 4:return 'jeudi';
-            case 5:return 'vendredi';
-            case 6:return 'samedi';
-            case 0:return 'dimanche';
-        }
-    });
-    
-    angular.module('dashboard').filter('monthOfYear', () => (input: Date) => {
-        switch (input.getMonth()){
-            case 1:return 'janvier';
-            case 2:return 'fevrier';
-            case 3:return 'mars';
-            case 4:return 'avril';
-            case 5:return 'mai';
-            case 6:return 'juin';
-            case 7:return 'juillet';
-            case 8:return 'août';
-            case 9:return 'septembre';
-            case 10:return 'octobre';
-            case 11:return 'novembre';
-            case 12:return 'décembre';
+        switch (input.getDay()) {
+            case 1: return 'lundi';
+            case 2: return 'mardi';
+            case 3: return 'mercredi';
+            case 4: return 'jeudi';
+            case 5: return 'vendredi';
+            case 6: return 'samedi';
+            case 0: return 'dimanche';
         }
     });
 
-    angular.module('dashboard').component('time', new TimeComponent());
+    angular.module('dashboard').filter('monthOfYear', () => (input: Date) => {
+        switch (input.getMonth()) {
+            case 1: return 'janvier';
+            case 2: return 'fevrier';
+            case 3: return 'mars';
+            case 4: return 'avril';
+            case 5: return 'mai';
+            case 6: return 'juin';
+            case 7: return 'juillet';
+            case 8: return 'août';
+            case 9: return 'septembre';
+            case 10: return 'octobre';
+            case 11: return 'novembre';
+            case 12: return 'décembre';
+        }
+    });
 }
