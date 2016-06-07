16b12a0da140f987a6e9c69cae7d8499d28ab5ae
Wilovent <william.klein@viacesi.fr>
2016-06-07 23:07:07 +0200

    background more complient to dev

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,6 +3,7 @@ import { Time } from './time/time'
 import { Weather } from './weather/weather'
 import { Meetings } from './meetings/meetings'
 import { NextStop } from './nextstop/nextstop'
+import { environment } from './environment';
 
 
 
@@ -12,4 +13,6 @@ import { NextStop } from './nextstop/nextstop'
   templateUrl: 'main.html',
   directives: [Time, Weather, Meetings, NextStop]
 })
-export class Main { }
+export class Main {
+  production: boolean = environment.production;
+}
